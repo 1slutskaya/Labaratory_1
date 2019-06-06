cdfecd595e4adcfd5eb76d1573dd307ea610c3c3
skuckaya5604@mail.ru <may-ed@mail.ru>
2019-06-06 23:40:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/labaratory_1/ListActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication.labaratory_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.example.myapplication.labaratory_1.adapter.LabRecyclerViewAdapter;
+import com.example.myapplication.labaratory_1.parser.StringToIntParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListActivity extends AppCompatActivity {
+
+    private List<Item> itemList;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        itemList = new ArrayList<>();
+        recyclerView= findViewById(R.id.itemsRecyclerView);
+
+        String str = StringToIntParser.getParsedString(196615);
+
+        for (int i = 1; i < 1000001; i++ ){
+            if(i%2 == 0){
+                itemList.add(new Item(i, "Grey"));
+            }
+            else{
+                itemList.add(new Item(i, "White"));
+            }
+        }
+
+        LabRecyclerViewAdapter adapter = new LabRecyclerViewAdapter(getApplicationContext(), itemList);
+        RecyclerView.LayoutManager  manager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+}
